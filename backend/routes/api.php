9f088a3e4d8d5d77c138b2e432912ece756b3132
Enrique-Feliczaki <enrique.zwetsch@gmail.com>
2024-10-29 11:02:43 -0300

    Implementado Controllers: company, user e job. Models configurados

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\JobController;
+use app\Http\Controllers\CompanyController;
+use app\Http\Controllers\UserController;
 
-
-Route::post('/register', [RegisterController::class, 'register']);
+Route::apiResource('users', UserController::class);
+Route::apiResource('companies', CompanyController::class);
+Route::apiResource('jobs', JobController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
