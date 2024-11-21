dda713472756cd937e90cef5a44078e775242052
Enrique-Feliczaki <enrique.zwetsch@gmail.com>
2024-11-21 14:06:51 -0300

    Adicionando mais categorias no Auth

--- backend/app/Domains/AuthDomain/Controllers/AuthController.php
+++ backend/app/Domains/AuthDomain/Controllers/AuthController.php
@@ -9,24 +9,19 @@ class AuthController extends Controller
 {
     protected $authService;
 
-
     public function __construct(AuthService $authService)
     {
         $this->authService = $authService;
     }
 
-
     public function login(Request $request)
     {
-
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-
         $user = $this->authService->login($request->email, $request->password);
-
 
         if (!$user) {
             return response()->json([
@@ -34,13 +29,21 @@ class AuthController extends Controller
             ], 401);
         }
 
-
         $token = $this->authService->generateToken($user);
 
-
+        // Retorna os dados do usuário junto com o token
         return response()->json([
             'message' => 'Login successful. Welcome!',
-            'token' => $token
+            'token' => $token,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'user_type' => $user->user_type,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+                // Inclua mais campos, se necessário
+            ],
         ]);
     }
 }
